1d0fd42fcdb2bc7f4d65dce84d6493623e26cd82
henrique-hartwig <henrique2805@gmail.com>
2024-05-05 21:07:26 -0300

    made tailwind work new page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-    <h1 className="Neflix text-2xl text-green-500">Netflix clone</h1>
+      <h1 className="text-2xl text-green-500">Netflix clone</h1>
     </>
   )
 }
---
+++ pages/auth.tsx
@@ -0,0 +1,10 @@
+import "../app/globals.css";
+
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-netflix-wallpaper bg-center bg-no-repeat bg-fixed">
+    </div>
+  )
+}
+
+export default Auth
