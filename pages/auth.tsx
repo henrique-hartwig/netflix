fc522cf86d9c1907cca5f6072eac6a72d6a9df6d
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-05-06 13:24:27 -0300

    few advances in auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,30 @@ import "../app/globals.css";
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-netflix-wallpaper bg-center bg-no-repeat bg-fixed">
+      <div className="bg-blue h-full w-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src='/images/logo.png' alt='Netflix Logo' className="h-12"/>
+        </nav>
+        <div className="flex justify-center ">
+          <div className="bg-black bg-opacity-70 self-center p-16 mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full ">
+            <header>
+              <h1 className="text-white text-4xl font-semibold">
+                Sign in
+              </h1>
+            </header>
+            <div className="text-white">
+              <p>
+                b
+              </p>
+            </div>
+            <footer>
+              <p className="text-grey-600">
+                aaaaaaaaa
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
