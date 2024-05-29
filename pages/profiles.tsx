53b03519be0fa3c5e2eb468bd70e4a7c409c5992
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-05-29 12:40:49 -0300

    fix: resolve logout

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,5 @@
-"use client";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -18,13 +17,12 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export default function Home() {
+const Profiles = () => {
   return (
-    <>
-      <h1 className="text-2xl text-green-500">Netflix clone</h1>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout
-      </button>
-    </>
+    <div>
+      <p className="text-white text-4xl">Profiles</p>
+    </div>
   );
-}
+};
+
+export default Profiles;
